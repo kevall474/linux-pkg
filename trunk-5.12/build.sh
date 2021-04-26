d53476a7d5a2c793a092fd8bd40abdf4461ca0ac
kevall474 <kevall474@tuta.io>
2021-04-26 17:42:06 -0400

    misc

--- trunk-5.12/build.sh
+++ trunk-5.12/build.sh
@@ -14,22 +14,6 @@ env _cpu_sched=2 makepkg -s
 
 rm -rf pkg/ src/
 
-env _cpu_sched=3 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=4 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=5 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=6 makepkg -s
-
-rm -rf pkg/ src/
-
 # clang build
 
 env _compiler=2 makepkg -s
@@ -41,21 +25,5 @@ env _cpu_sched=1 _compiler=2 makepkg -s
 rm -rf pkg/ src/
 
 env _cpu_sched=2 _compiler=2 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=3 _compiler=2 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=4 _compiler=2 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=5 _compiler=2 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=6 _compiler=2 makepkg -s
 
 rm -rf pkg/ src/
