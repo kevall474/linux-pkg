c5369c2596d6be40bd8395f83600f06a3fd88454
kevall474 <kevall474@tuta.io>
2021-06-28 22:50:17 -0400

    linux 5.13

--- stable/build.sh
+++ stable/build.sh
@@ -22,10 +22,6 @@ env _cpu_sched=4 makepkg -s
 
 rm -rf pkg/ src/
 
-env _cpu_sched=5 makepkg -s
-
-rm -rf pkg/ src/
-
 # clang build
 
 env _compiler=2 makepkg -s
@@ -45,9 +41,5 @@ env _cpu_sched=3 _compiler=2 makepkg -s
 rm -rf pkg/ src/
 
 env _cpu_sched=4 _compiler=2 makepkg -s
-
-rm -rf pkg/ src/
-
-env _cpu_sched=5 _compiler=2 makepkg -s
 
 rm -rf pkg/ src/
